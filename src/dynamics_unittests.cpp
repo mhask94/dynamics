df70b22905b1d423b475ce1b8657780198f3e9df
Mat Haskell <mhaskell9@gmail.com>
2018-11-17 20:58:06 -0700

    Added two tests for dynamics

--- src/dynamics_unittests.cpp
+++ src/dynamics_unittests.cpp
@@ -1,15 +1,29 @@
 #include "gtest/gtest.h"
 #include "drone.hpp"
 
-TEST(Quadcopter,givenEquilibriumInputs_DoesNotMove)
+TEST(QuadcopterAtEquilibrium,givenEquilibriumInputs_DoesNotMove)
 {
     dyn::Drone Quadcopter;
-//    Quadcopter.x = States_t{0,0,0,0,0,0,0,0,0,0,0,0};
-//    Input u{0.55,0.55,0.55,0.55};
-//    Quadcopter.propagateDynamics(u);
+    dyn::uVec u{0.55,0.55,0.55,0.55};
+    Quadcopter.sendMotorCmds(u);
 
-//    States_t expected_states{0,0,0,0,0,0,0,0,0,0,0};
-//    States_t actual_states{Quadcopter.getStates()};
+    dyn::xVec expected_states;
+    expected_states.setZero(dyn::STATE_SIZE,1);
+    dyn::xVec actual_states{Quadcopter.getStates()};
 
-//    EXPECT_EQ(expected_states,actual_states);
+    EXPECT_EQ(expected_states,actual_states);
+}
+
+TEST(QuadcopterAtEquilibrium,givenAboveEquilibriumInputs_MovesUp)
+{
+    dyn::Drone Quadcopter;
+    dyn::uVec u{0.8,0.8,0.8,0.8};
+    Quadcopter.sendMotorCmds(u);
+
+    dyn::xVec expected_states;
+    expected_states.setZero(dyn::STATE_SIZE,1);
+
+    dyn::xVec actual_states{Quadcopter.getStates()};
+
+    EXPECT_EQ(expected_states,actual_states);
 }
