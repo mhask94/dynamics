d1e7318e5618ef0ca7ef6362c819189e6d812eae
Mat Haskell <mhaskell9@gmail.com>
2018-12-08 13:21:37 -0700

    Working on integrating CVXGEN

--- include/controller.hpp
+++ include/controller.hpp
@@ -1,7 +1,12 @@
 #ifndef CONTROLLER_HPP
 #define CONTROLLER_HPP
 
+extern "C"
+{
+#include "solver.h"
+}
 #include "types.hpp"
+#include "nav_msgs/Odometry.h"
 
 class Controller
 {
@@ -9,6 +14,18 @@ public:
     Controller();
     virtual ~Controller();
     dyn::uVec calculateControl(const dyn::xVec &states);
+
+protected:
+    void load_data();
+
+private:
+    Vars m_vars;
+    Params m_params;
+    Workspace m_ws;
+    Settings m_settings;
+    dyn::xVec m_x;
+    dyn::xVec m_ref;
+    dyn::uVec m_u;
 };
 
 #endif // CONTROLLER_HPP
